982547d1715f0cad2a007e2a28224882d409cb1f
DainJI <114410999+dainji@users.noreply.github.com>
2023-12-19 17:00:47 +0900

    run main.py

---
+++ run/main.py
@@ -0,0 +1,24 @@
+import yaml
+import subprocess
+
+
+def execute_tasks(task_list):
+    for task in task_list:
+        print(f"Executing {task['name']}...")
+        commands = task.get('commands')
+        if commands:
+            for command in commands:
+                subprocess.run(command.split(), check=True)
+
+
+def main():
+    # Load tasks from the YAML file
+    with open('tasks.yaml', 'r') as file:
+        tasks = yaml.safe_load(file)
+
+    # Execute tasks
+    execute_tasks(tasks.get('tasks', []))
+
+
+if __name__ == "__main__":
+    main()
