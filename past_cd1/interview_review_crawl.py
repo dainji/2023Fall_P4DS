560b77940bbe6f3ce3c01fac0f118e17da7e98cb
dain ji <dain@s-MacBook-Pro.local>
2023-12-19 13:38:34 +0900

    Initial commit

--- past_cd1/interview_review_crawl.py
+++ past_cd1/interview_review_crawl.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
 
 response = requests.get('https://www.jobkorea.co.kr/starter/review/view?C_Idx=489&Ctgr_Code=3&FavorCo_Stat=0&schTxt=%EA%B5%90%EB%B3%B4%EB%AC%B8%EA%B3%A0&G_ID=0&Page=1')
 soup = BeautifulSoup(response.text, 'html.parser')
